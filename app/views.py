6f56574b031321db970bd75a88243cfbfe48295d
srihari128 <csrihari@gmail.com>
2023-03-31 15:58:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Hari(request):
+    return render(request,'Hari.html')
+
+def pspk(request):
+    return render(request,'pspk.html')
